77ed9060e178a1237d41fc8dd651b3279a2e5563
Navya Sahay <navyasahay@Navyas-MacBook-785.local>
2025-01-25 16:30:05 -0500

    added comments and corrected the overall windows function for analysis_navya

--- analysis_navya.py
+++ analysis_navya.py
@@ -10,7 +10,7 @@ data = pd.read_csv(data_path)
 data_path2 = os.path.join(os.path.dirname(__file__),'data/filt_hs92_country_product_year_2.csv')
 original_data = pd.read_csv(data_path2)
 
-
+# if we are excluding china in addition to the existing countries for exclusion criteria:
 exc_countries_china = ["australia", "austria", "belgium", "canada", "chile", "colombia",
                  "costa_rica", "czechia", "denmark", "estonia", "finland", "france",
                  "germany", "greece", "hungary", "iceland", "ireland", "israel",
@@ -21,7 +21,7 @@ exc_countries_china = ["australia", "austria", "belgium", "canada", "chile", "co
                  "us_minor_outlying_islands", "china", "hong_kong", "macao"]
 
 
-
+#if we are not excluding china:
 exc_countries = ["australia", "austria", "belgium", "canada", "chile", "colombia",
                  "costa_rica", "czechia", "denmark", "estonia", "finland", "france",
                  "germany", "greece", "hungary", "iceland", "ireland", "israel",
@@ -31,7 +31,7 @@ exc_countries = ["australia", "austria", "belgium", "canada", "chile", "colombia
                  "turkiye", "united_kingdom", "united_states_of_america", "us_virgin_islands",
                  "us_minor_outlying_islands"]
 
-
+#excluded goods for exclusion criteria
 exc_goods = ["ores_slag_and_ash", "mineral_fuels,_oils_and_waxes", "precious_metals_and_stones",
              "iron_and_steel", "articles_of_iron_or_steel", "copper", "nickel", "aluminum",
              "lead", "zinc", "tin", "other_base_metals", "miscellaneous_articles_of_base_metal"]
@@ -40,12 +40,13 @@ exc_goods = ["ores_slag_and_ash", "mineral_fuels,_oils_and_waxes", "precious_met
 def entire_time_period_ranking_shift(input_data: pd.DataFrame, rank_column_name: str, start: int, end: int, modification: bool, china: bool):
     '''Given a start and end year, this function  calculates the rank shift based on a given ranking system for each country-product pair '''
 
+    #if china boolean is false, we are excluding china
     if not china:
         oecd = [156,344,446]
         df_mask = input_data['country_id'].isin(oecd)
         input_data = input_data[~df_mask]
 
-
+    #if modification is true then we want to filter for the top countries whose ranking is at most 30 as of 2022 - the final year:
     if modification:
         filtered_data = input_data[(input_data['year'] == 2022) & (input_data[rank_column_name] < 30)]
         filtered_countries = filtered_data['country'].tolist()
@@ -53,16 +54,22 @@ def entire_time_period_ranking_shift(input_data: pd.DataFrame, rank_column_name:
         clean_data = input_data[(input_data['country'].isin(filtered_countries)) & (input_data['name_short_en'].isin(filtered_products))]
         grouped = clean_data.groupby(['country','name_short_en'])
 
+
+    #if modification is false, we are using the original data without running the filters in modification.py or any additional filters: 
     else:    
         grouped = input_data.groupby(['country','name_short_en'])
 
+
+    #new dataframe to populate after the ranking shift calculation for sector successes is done:
     new_data = pd.DataFrame(columns=['country', 'product', 'hs_code' , f'{start}-{end}_rank_shift'])
     country_code_list = []
     product_code_list = []
     hs_code_list =[]
     ranking_shift_list = []
 
+    #in each country, product group we are calculating the rank shift overall usually when start=1995, end=2022
     for name, group in grouped:
+        #if china is to be excluded or not: 
         if china:
             excluded_countries = exc_countries
         else:
@@ -75,6 +82,8 @@ def entire_time_period_ranking_shift(input_data: pd.DataFrame, rank_column_name:
         country_code_list.append(name[0])
         product_code_list.append(name[1])
         hs_code_list.append(group['product_code'].iloc[0])
+        
+        #each country, product for a particular year should only appear once for the ranking shift calculation to work: 
         if(len(group[group['year'] == end]) == 1) and (len(group[group['year'] == start]) == 1):
             end_ranking = group[group['year'] == end][rank_column_name].iloc[0]
             beg_ranking = group[group['year'] == start][rank_column_name].iloc[0]
@@ -84,18 +93,21 @@ def entire_time_period_ranking_shift(input_data: pd.DataFrame, rank_column_name:
             rank_shift = None
         ranking_shift_list.append(rank_shift)
 
+    #populate the series for the dataframe
     new_data['country'] = country_code_list
     new_data['product'] = product_code_list
     new_data['hs_code'] = hs_code_list
     new_data[f'{start}-{end}_rank_shift'] = ranking_shift_list
+
+    #drop the ranking shifts that are None out of the dataframe as they would interfere with the sorting logic:
     new_data = new_data.dropna(subset=[f'{start}-{end}_rank_shift'])
     
     return new_data
 
 
 
-#Function to look at the big ranking shifts and drops for smaller windows with the 500 success stories
-def window_time_period_ranking_shift(time_window: int, rank_column_name: str, modification: bool, china: bool):
+#Function to look at the big ranking shifts and drops for smaller windows within the 500 success stories
+def window_time_period_ranking_shift(input_data: pd.Dataframe, time_window: int, rank_column_name: str, modification: bool, china: bool):
     '''Calculates small rank shifts given a window size for all windows between 1995-20005
     '''
 
@@ -108,9 +120,7 @@ def window_time_period_ranking_shift(time_window: int, rank_column_name: str, mo
     
     all_windows_data = pd.DataFrame()
     for start, end in windows:
-        window_data = entire_time_period_ranking_shift(data, rank_column_name, start, end, modification,china)
-        #if we want to run it on the original data: 
-        # window_data = entire_time_period_ranking_shift(original_data, rank_column_name, start, end)
+        window_data = entire_time_period_ranking_shift(input_data, rank_column_name, start, end, modification,china)
         all_windows_data = pd.merge(
             all_windows_data, window_data, on=['country', 'product', 'hs_code'], how='outer'
         ) if not all_windows_data.empty else window_data
@@ -137,13 +147,16 @@ def generate_outputs_plots(rank_metric: str, modification: bool, china: bool):
     #If we want data with the modification filters use this:
     if modification:
         overall_time_period = entire_time_period_ranking_shift(data, rank_metric, 1995,2022, modification,china)
+        windows_overall = window_time_period_ranking_shift(data,5, rank_metric,modification,china)
         if china:
             directory_name = "mod_with_china_"
         else:
             directory_name = "mod_no_china_"
     
+    #else: use the original_data:
     else:
         overall_time_period = entire_time_period_ranking_shift(original_data, rank_metric, 1995,2022,modification,china)
+        windows_overall = window_time_period_ranking_shift(original_data, 5, rank_metric,modification,china)
         if china:
             directory_name = "china_"
         else:
@@ -152,14 +165,14 @@ def generate_outputs_plots(rank_metric: str, modification: bool, china: bool):
     #Gets the top 500 sector success stories
     fivehundred_sector_successes = overall_time_period.sort_values(by='1995-2022_rank_shift', ascending=False).head(500)
 
-    windows_overall = window_time_period_ranking_shift(5, rank_metric,modification,china)
+    #gets the detailed rank shifts for each window of 5 years within the 500 sector successes:
     detailed_five_hundred = windows_overall.merge(fivehundred_sector_successes, on=['country', 'product', 'hs_code'], how='inner')
     detailed_five_hundred_sorted = detailed_five_hundred.sort_values('1995-2022_rank_shift', ascending=False)
 
-
+    #create a folder for the plots: 
     os.makedirs(f'{directory_name}{rank_metric}_sector_successes_plots', exist_ok=True)
 
-
+    #to plot the top 50 sector successes for the rank metric to visualize the data and top stories:
     for index, row in fivehundred_sector_successes.head(20).iterrows():
         ranks = data[(data['country'] == row['country']) & (data['name_short_en'] == row['product'])]
         year_ranks = ranks.sort_values(by='year', ascending=True)
--- modifications.py
+++ modifications.py
@@ -3,17 +3,19 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-
-
 #takes in preprocessed data that has been filtered
 original_data_path = os.path.join(os.path.dirname(__file__),'data/filt_hs92_country_product_year_2.csv')
 original_data = pd.read_csv(original_data_path)
 
 
-# adding the eci filter
+# Adding the eci filter
+
+#gets the eci rankings from rankings.tab in references:
 eci_ranking = pd.read_csv('references/rankings.tab', sep='\t')
 directory = os.path.dirname('references/rankings.tab')
 eci_groups = eci_ranking.groupby('country_id')
+
+#creates and populates the new dataframe for eci ranking shifts from 1995 to 2022: 
 new_eci_ranking = pd.DataFrame(columns=['country_id', 'eci_ranking_shift'])
 eci_countries = []
 eci_ranking_shifts = []
@@ -30,12 +32,16 @@ new_eci_ranking['country_id'] = eci_countries
 new_eci_ranking['eci_ranking_shift'] = eci_ranking_shifts
 new_eci_ranking = new_eci_ranking.dropna(subset=['eci_ranking_shift'])
 new_eci_file = os.path.join(directory, 'eci_rank_shifts.csv')
+
+#sorts the new dataframe by the biggest eci ranking shifts and filters for the top 50 countries: 
 new_eci_ranking = new_eci_ranking.sort_values(by='eci_ranking_shift', ascending=False)
 new_eci_ranking.to_csv(new_eci_file,index=False)
 top50countries = new_eci_ranking.head(50)
 new_eci_countryids = top50countries['country_id'].tolist()
 original_data = original_data[original_data['country_id'].isin(new_eci_countryids)]
 
+
+#only keeps the top 50 countries with the best eci ranking shifts for the country product year and saves it in a new csv:
 clean_data_file_path = os.path.join('data', "clean_hs92_country_product_year_2.csv")
 original_data.to_csv(clean_data_file_path, index=False)
 
